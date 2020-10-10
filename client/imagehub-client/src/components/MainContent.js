8b9adec2ff3aabccd44febf6bb93f5b8c7b44d61
tboelcskey <bolcskey.tamas@gmail.com>
2020-10-10 17:13:36 +0200

    Created navbar in main content

--- client/imagehub-client/src/components/MainContent.js
+++ client/imagehub-client/src/components/MainContent.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import Feed from "./Feed";
+import Profile from "./Profile";
+import Upload from "./Upload";
+import MenuAppBar from "./MenuAppBar.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Search from "./Search";
 
 export default function MainContent() {
-    return (
-        <div>
-            Maincontent
-        </div>
-    )
+  return (
+    <Router>
+      <div>
+        <MenuAppBar />
+        <Switch>
+          <Route path="/feed" component={Feed} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/upload" component={Upload} />
+          <Route path="/search" component={Search} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
