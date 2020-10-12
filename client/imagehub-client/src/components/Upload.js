87820b0d06a38e8cdd1fd96a3b5fe9eda325c41a
tboelcskey <bolcskey.tamas@gmail.com>
2020-10-12 22:33:47 +0200

    removed imports

--- client/imagehub-client/src/components/Upload.js
+++ client/imagehub-client/src/components/Upload.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import "./Upload.css";
-import Button from "@material-ui/core/Button";
-import { makeStyles } from "@material-ui/core/styles";
 import UploadImageButton from "./UploadImageButton";
 import ImageSelector from "./ImageSelector";
 
