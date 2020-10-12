0cf9a6ea0a8b228310f72008afe5587d8cbaee3f
tboelcskey <bolcskey.tamas@gmail.com>
2020-10-12 22:35:55 +0200

    Facebook login added to welcome page

--- client/imagehub-client/src/components/Login.js
+++ client/imagehub-client/src/components/Login.js
@@ -1,9 +1,8 @@
 import React from "react";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
 import "./Login.css";
+import Facebook from "./Facebook";
 
 export default function Login(props) {
   return (
@@ -16,17 +15,8 @@ export default function Login(props) {
               Üdvözlünk az ImageHub oldalán! A továbblépéshez használd a
               facebookodat!
             </p>
+            <Facebook setIsLoggedIn={props.setIsLoggedIn} />
           </CardContent>
-          <CardActions>
-            <Button
-              className="button--login"
-              variant="contained"
-              onClick={() => props.setIsLoggedIn(true)}
-              color="primary"
-            >
-              Belépés
-            </Button>
-          </CardActions>
         </div>
       </Card>
     </div>
