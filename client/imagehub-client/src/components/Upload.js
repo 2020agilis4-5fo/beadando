e259ec1bc9011a347fb73ca054ee25f31da322da
tboelcskey <bolcskey.tamas@gmail.com>
2020-10-12 20:02:58 +0200

    Upload page with image selection

--- client/imagehub-client/src/components/Upload.js
+++ client/imagehub-client/src/components/Upload.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Upload.css";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import UploadImageButton from "./UploadImageButton";
+import ImageSelector from "./ImageSelector";
 
 export default function Upload() {
-    return (
-        <h1>Upload</h1>
-    )
+  let [image, setImage] = useState();
+  let [selected, setSelected] = useState(false);
+
+  return (
+    <div className="container--upload">
+      <div className="upload__wrapper">
+        <ImageSelector image={image} setImage={setImage} setSelected={setSelected}/>
+        {selected && <UploadImageButton image={image} />}
+      </div>
+    </div>
+  );
 }
