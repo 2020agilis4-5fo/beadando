d504d5be6bef8f1a9b67fce671c8d096062f11b3
tboelcskey <bolcskey.tamas@gmail.com>
2020-10-14 01:50:57 +0200

    Search page textfield added

--- client/imagehub-client/src/components/Search.js
+++ client/imagehub-client/src/components/Search.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import "./Profile.css";
+import "./Feed.css";
+import { TextField } from "@material-ui/core";
 
 export default function Search() {
-    return (
-        <h1>Search</h1>
-    )
+  return (
+    <div>
+      <div className="container--profile">
+        <h1>Felhasználó keresés</h1>
+      </div>
+      <div className="container--feed">
+        <TextField
+          id="outlined-secondary"
+          label="Felhasználó neve:"
+          variant="outlined"
+          color="secondary"
+        />
+      </div>
+    </div>
+  );
 }
