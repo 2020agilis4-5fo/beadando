25c31f943ae42d3d8d32ffbc4171adb7fcb1c220
tboelcskey <bolcskey.tamas@gmail.com>
2020-10-14 01:43:11 +0200

    Feed added to Profile

--- client/imagehub-client/src/components/Profile.js
+++ client/imagehub-client/src/components/Profile.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import Feed from "./Feed";
+import "./Profile.css";
 
-export default function Profile() {
-    return (
-        <h1>Profile</h1>
-    )
+export default function Profile(props) {
+  console.log(props.userData);
+  return (
+    <div>
+      <div className="container--profile">
+        <h1>{props.userData.name}</h1>
+        <h2>{props.userData.email}</h2>
+      </div>
+      <Feed />
+    </div>
+  );
 }
