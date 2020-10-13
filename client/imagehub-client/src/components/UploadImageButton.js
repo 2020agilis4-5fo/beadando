d4df35fd478011e8601ce95849425162d9bd11b6
tboelcskey <bolcskey.tamas@gmail.com>
2020-10-13 23:05:00 +0200

    Image upload to backend added

--- client/imagehub-client/src/components/UploadImageButton.js
+++ client/imagehub-client/src/components/UploadImageButton.js
@@ -1,13 +1,24 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import SaveIcon from "@material-ui/icons/Save";
+import axios from "axios";
 
+function UploadImage(props) {
+  axios
+    .post("https://localhost:44380/api/image", {
+      base64EncodedImage: props.image.split(",")[1],
+      imageNameWithExtension: Math.random().toString(36).substring(7) + ".jpg",
+    })
+    .then((res) => {
+      props.setSelected(false);
+    });
+}
 
 export default function UploadImageButton(props) {
   return (
-      
     <div>
       <Button
+        onClick={() => UploadImage(props)}
         variant="contained"
         color="secondary"
         startIcon={<SaveIcon />}
