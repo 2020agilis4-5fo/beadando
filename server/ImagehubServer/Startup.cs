71f7d45ca2f08ab8252589ffd0734676dfbb3dad
Dan <deseatnikov.daniel@gmail.com>
2020-11-10 22:34:15 +0100

    Startup deprecated fix

--- server/ImagehubServer/Startup.cs
+++ server/ImagehubServer/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Repository.Implementations;
 using Repository.Interfaces;
 using Services.Implementations;
@@ -108,7 +109,7 @@ namespace ImagehubServer
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
