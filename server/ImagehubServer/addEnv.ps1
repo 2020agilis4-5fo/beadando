33ff099aba4a902384842dc475f803ce7dca297e
Dan <deseatnikov.daniel@gmail.com>
2020-12-09 01:18:02 +0100

    envscript change but azure needs to be set separately anyway

--- server/ImagehubServer/addEnv.ps1
+++ server/ImagehubServer/addEnv.ps1
@@ -1,4 +1,4 @@
-﻿[System.Environment]::SetEnvironmentVariable('IMG_HUB_CONN','Server=DESKTOP-238VLLK;Database=ImageHub;Trusted_Connection=True;MultipleActiveResultSets=true',
+﻿[System.Environment]::SetEnvironmentVariable('IMG_HUB_CONN','Server=tcp:dbdev45.database.windows.net,1433;Initial Catalog=45dev;Persist Security Info=False;User ID=ddani;Password=Asdasd123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=500;',
 [System.EnvironmentVariableTarget]::Machine)
 [System.Environment]::SetEnvironmentVariable('IMG_HUB_FB_ID','2122090571268724',
 [System.EnvironmentVariableTarget]::Machine)
